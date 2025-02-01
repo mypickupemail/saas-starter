42d1394475a6fbfdab6fc7513709bfdb42b80149
liuhuapiaoyuan <278780765@qq.com>
2025-02-02 03:38:13 +0800

    feat(auth): enhance user creation with team and activity logging

--- lib/auth/PrismaAdapter.ts
+++ lib/auth/PrismaAdapter.ts
@@ -1,35 +1,47 @@
 import { PrismaAdapter as BasePrismaAdapter } from "@auth/prisma-adapter";
 import db from "../db";
+import { logActivity } from "../db/queries";
+import { ActivityType } from "../db/schema";
 
-type PrismaAdapterArg = Parameters<typeof BasePrismaAdapter>[0]
-type PrismaAdapterReturn = ReturnType<typeof BasePrismaAdapter>
-
+type PrismaAdapterArg = Parameters<typeof BasePrismaAdapter>[0];
+type PrismaAdapterReturn = ReturnType<typeof BasePrismaAdapter>;
 
 /**
- * config user role 
- * @param arg 
- * @returns 
+ * config user role
+ * @param arg
+ * @returns
  */
 export function PrismaAdapter(arg: PrismaAdapterArg) {
-    const { createUser: _createUser, ...adapter } = BasePrismaAdapter(arg)
-    const createUser: PrismaAdapterReturn['createUser'] = _createUser ? async (user) => {
-        // @ts-expect-error adapter role 
-        user.role = 'owner'
-        const createUser = await _createUser(user)
-        const email = createUser.email
+  const { createUser: _createUser, ...adapter } = BasePrismaAdapter(arg);
+  const createUser: PrismaAdapterReturn["createUser"] = _createUser
+    ? async (user) => {
+        // @ts-expect-error adapter role
+        user.role = "owner";
+        const createdUser = await _createUser(user);
+        const email = createdUser.email;
         const newTeam = {
-            name: `${email}'s Team`,
+          name: `${email}'s Team`,
         };
-        await db.team.create({
-            data: {
-                name: newTeam.name
-            }
-        })
-        return createUser
-    } : undefined
+        const team = await db.team.create({
+          data: {
+            name: newTeam.name,
+          },
+        });
+        await db.teamMember.create({
+          data: {
+            userId: createdUser.id,
+            teamId: team.id,
+            role: "owner",
+          },
+        });
+        await logActivity(team.id, createdUser.id, ActivityType.SIGN_UP);
 
-    return {
-        createUser,
-        ...adapter
-    }
+        return createdUser;
+      }
+    : undefined;
+
+  return {
+    createUser,
+    ...adapter,
+  };
 }
